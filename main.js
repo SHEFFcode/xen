94c0150bf1bb9218382f349268fb0d336dfe61bf
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-27 10:55:34 -0700

    first setup for xen project

---
+++ main.js
@@ -0,0 +1,28 @@
+//Mobile menu collapse
+$(document).ready(function () {
+  $(".navbar-nav li a").click(function(event) {
+    $(".navbar-collapse").collapse('hide');
+  });
+});
+
+//Menu Bar Darkening
+$(window).scroll(function(){
+  if($('.navbar').offset().top > 50) {
+    $('.navbar-fixed-top').addClass('top-nav-collapse');
+  } else {
+    $('.navbar-fixed-top').removeClass('top-nav-collapse');
+  }
+});
+
+
+//Scrolling
+$(function(){
+  $('.page-scroll a').bind('click', function(){
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top,
+
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+});
